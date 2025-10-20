8fc4c6e798b0a308debd70289785de93af1ad96d
gpdjwork-svg <gpdjwork@gmail.com>
2025-10-20 17:28:39 -0600

    hmwrk 9 made and moved

---
+++ Homework 9/sketch.js
@@ -0,0 +1,26 @@
+ function setup()
+    {
+        createCanvas(600,600);
+    }
+function draw()
+    {
+        background(220);
+        fill(0,0,0,)
+        textSize(32);
+        text('Self Portrait',10,30);
+        text('By Grace',10,60)
+        fill(250,240,190)
+        ellipse(300,200,225,390);
+        fill(255,182,193)
+        triangle(150, 500, 300, 160, 450, 500);
+        fill(211,188,180)
+        circle(300,100,150);
+        line(270,225,100,400);
+        line(330,225,500,400);
+        line(273,140,327,140)
+        point(270,85);
+        point(330,85)
+        strokeWeight(10);
+        rect(220,500,50,100);
+        rect(320,500,50,100);
+    }
